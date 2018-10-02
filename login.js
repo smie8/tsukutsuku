592a5a93e679762164d03a3cd40cd68e4f15c2a0
Nfagerho <Nooa.Fagerholm@wippies.fi>
2018-10-02 15:06:18 +0300

    Poistettiin idea.filet

--- login.js
+++ login.js
@@ -1,12 +1,13 @@
 
-function store() {
+function rekisterointi() {
 
     var inputUsername = document.getElementById('username').value;
     var inputPassword = document.getElementById('password').value;
 
     var kirjautumistiedot = { //JSON olio
         käyttäjänimi: inputUsername,
-        salasana: inputPassword
+        salasana: inputPassword,
+        vari: "pun"
     };
     if (!localStorage.getItem(inputUsername)) {
         localStorage.setItem(inputUsername, JSON.stringify(kirjautumistiedot));
@@ -17,18 +18,27 @@ function store() {
 
 }
 
+function kirjautuminen() {
 
-function login() {
     var inputUsername= document.getElementById('username2');
     var inputPassword= document.getElementById('password2');
     var username = inputUsername.value;
     var password = inputPassword.value;
     var paikallinenData = JSON.parse(localStorage.getItem(username));
     if ((username == paikallinenData.käyttäjänimi) && (password == paikallinenData.salasana)) {
+        var kirjautunut = paikallinenData.käyttäjänimi;
+        localStorage.setItem('kirjautunutNyt', kirjautunut);
         alert("Tervetuloa palveluun " + paikallinenData.käyttäjänimi + "!");
         window.location.href = "Aloitus.html";
         return false;
     } else {
         alert("Virheellinen käyttäjätunnus ja/tai salasana");
     }
-}// end login()
+    // end login()
+}
+
+function preferenssi() {
+    var paikallinenData = localStorage.getItem('kirjautunutNyt');
+    
+
+}
