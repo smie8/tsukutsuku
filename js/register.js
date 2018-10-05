d61b393ba2e7f8accbd5177cbdcd1c217a8f076a
Nfagerho <Nooa.Fagerholm@wippies.fi>
2018-10-05 09:40:13 +0300

    Lisätty rekisteröintisivulle vaatimuksia salasanan muodosta

--- js/register.js
+++ js/register.js
@@ -2,6 +2,7 @@
 function rekisterointi() {
     var inputUsername = document.getElementById('username').value;
     var inputPassword = document.getElementById('password').value;
+    var regularExpression = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
 
     var kirjautumistiedot = {
         käyttäjänimi: inputUsername,
@@ -11,13 +12,17 @@ function rekisterointi() {
         paate: ""
     };
     // Tarkistetaan, onko käyttäjätunnusta olemassa ennestään
-    if (!localStorage.getItem(inputUsername)) {
-        localStorage.setItem(inputUsername, JSON.stringify(kirjautumistiedot));
-        alert("Rekisteröinti onnistui!");
-        localStorage.setItem('kirjautunutNyt', inputUsername);
-        window.location.href = "index.html";
-        return false;
-    } else {
-        alert("Antamasi käyttäjätunnus on jo käytössä")
+    if(!regularExpression.test(inputPassword)) {
+        alert("Salasanassa tulee olla vähintään yksi erikoismerkki ja yksi numero");
+    }else {
+        if (!localStorage.getItem(inputUsername)) {
+            localStorage.setItem(inputUsername, JSON.stringify(kirjautumistiedot));
+            alert("Rekisteröinti onnistui!");
+            localStorage.setItem('kirjautunutNyt', inputUsername);
+            window.location.href = "index.html";
+            return false;
+        } else {
+            alert("Antamasi käyttäjätunnus on jo käytössä")
+        }
     }
 }
